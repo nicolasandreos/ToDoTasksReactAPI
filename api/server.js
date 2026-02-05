f02807886b8bd893b41b2b6502964935e9594c27
Nicolas <nicolasandreose@gmail.com>
2026-02-05 20:14:02 -0300

    fix

---
+++ api/server.js
@@ -0,0 +1,23 @@
+const jsonServer = require('json-server');
+const fs = require('fs');
+const path = require('path');
+
+const server = jsonServer.create();
+
+const filePath = path.join('db.json');
+const data = fs.readFileSync(filePath, 'utf-8');
+const db = JSON.parse(data);
+
+const router = jsonServer.router(db);
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(jsonServer.rewriter({
+  '/api/*': '/$1',
+}));
+server.use(router);
+server.listen(3000, () => {
+  console.log('JSON Server is running on port 3000');
+});
+
+module.exports = server;
