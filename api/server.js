dac1cb74d69bda3f687b9acf141d9bc29598e1bc
Nicolas <nicolasandreose@gmail.com>
2026-02-04 22:48:54 -0300

    fix again

--- api/server.js
+++ api/server.js
@@ -1,20 +1,18 @@
-const jsonServer = require('json-server');
-const fs = require('fs');
-const path = require('path');
+import jsonServer from "json-server";
+import fs from "fs";
+import path from "path";
 
 const server = jsonServer.create();
-
-const filePath = path.join(process.cwd(), 'db.json');
-const data = fs.readFileSync(filePath, 'utf-8');
-const db = JSON.parse(data);
-
-const router = jsonServer.router(db);
+const router = jsonServer.router(
+  JSON.parse(
+    fs.readFileSync(path.join(process.cwd(), "db.json"), "utf-8")
+  )
+);
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(jsonServer.rewriter({
-  '/api/*': '/$1',
-}));
-server.use(router);
+server.use("/api", router);
 
-module.exports = server;
+export default function handler(req, res) {
+  server(req, res);
+}
